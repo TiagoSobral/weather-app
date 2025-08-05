006b38f18848a24993c93e1348b9e7f71ea68ad8
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-05 14:50:22 +0100

    (feat) getCurrentData
    -> gets data required to be used for current and
    forecast weather.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -12,3 +12,16 @@ const getWeatherFrom = async function getLocationWeather(location) {
 
 	return { data };
 };
+
+const getCurrentData = async function getLocationWeatherData(location) {
+	const weatherInfo = await getWeatherFrom(location);
+
+	let address = weatherInfo.data.resolvedAddress;
+	let temperature = weatherInfo.data.currentConditions.temp;
+	let condition = weatherInfo.data.currentConditions.conditions;
+	let highTemp = weatherInfo.data.days[0].tempmax;
+	let lowTemp = weatherInfo.data.days[0].tempmin;
+	let forecast = weatherInfo.data.days;
+
+	return { address, temperature, condition, highTemp, lowTemp, forecast };
+};
