24049f317f314097458e6115d9c0ff496380298c
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-05 14:47:38 +0100

    (feat) getWeatherFrom (gets weather from fetch)

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,14 @@
-import "../css/style.css"
+import '../css/style.css';
+
+const getWeatherFrom = async function getLocationWeather(location) {
+	const promise = await fetch(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=BJAE6JWBDH7WPY5BL3ZEC9MUC`,
+		{ mode: 'cors' }
+	);
+
+	const data = await promise.json();
+
+	console.log(data);
+
+	return { data };
+};
