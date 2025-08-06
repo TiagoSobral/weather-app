d89c5839a3a9a3a83a5387635627fc4eb0ecfe5c
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-06 18:35:58 +0100

    (refactor) getCurrentData, getForecastData
    -> getCurrentData now calls the class
    CurrentWeather inside it, with all the info received
    from the argument the function takes.
    It only returns the object instead of all the variables
    previously.
    
    -> getForecastData calls the class DailyWeather
    inside it instead of creating an object literal.
    
    (refactor) locationWeather
    
    -> gets all the data calls the necessary functions
    creates a new class Location and then the objects
    that come from getCurrentData, getForecastData and getHourlyData
    are assigned to it with Object.assign.
    
    (feat) getHourlyData
    
    -> creates an array of new HourlyWeather classes, that
    are returned to be assigned to locationWeather.

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -1,4 +1,9 @@
-import { CurrentWeather, Location } from './classes';
+import {
+	CurrentWeather,
+	DailyWeather,
+	HourlyWeather,
+	Location,
+} from './classes';
 
 const getWeatherFrom = async function getLocationWeather(location, unit) {
 	const promise = await fetch(
@@ -12,13 +17,14 @@ const getWeatherFrom = async function getLocationWeather(location, unit) {
 };
 
 const getCurrentData = function getLocationWeatherData(data) {
-	let address = data.resolvedAddress;
 	let temperature = data.currentConditions.temp;
 	let condition = data.currentConditions.conditions;
 	let highTemp = data.days[0].tempmax;
 	let lowTemp = data.days[0].tempmin;
 
-	return { address, temperature, condition, highTemp, lowTemp };
+	const current = new CurrentWeather(temperature, condition, highTemp, lowTemp);
+
+	return { current };
 };
 
 const getForecastData = function getWeatherForecast(dataForecast) {
@@ -27,12 +33,12 @@ const getForecastData = function getWeatherForecast(dataForecast) {
 	for (let day = 0; day < 7; day++) {
 		let currentDay = dataForecast[day];
 
-		let dayInfo = {
-			date: currentDay.datetime,
-			highTemp: currentDay.tempmax,
-			lowTemp: currentDay.tempmin,
-			condition: currentDay.conditions,
-		};
+		let dayInfo = new DailyWeather(
+			currentDay.datetime,
+			currentDay.tempmax,
+			currentDay.tempmin,
+			currentDay.conditions
+		);
 
 		forecast.push(dayInfo);
 	}
@@ -40,27 +46,41 @@ const getForecastData = function getWeatherForecast(dataForecast) {
 	return { forecast };
 };
 
+const getHourlyData = function getWeatherHourly(hourlyData) {
+	let hours = hourlyData.map((hour) => {
+		let currentHour = hour;
+
+		let hourInfo = new HourlyWeather(
+			currentHour.datetime,
+			currentHour.conditions,
+			currentHour.temp
+		);
+
+		return hourInfo;
+	});
+
+	return { hours };
+};
+
 export const locationWeather = async function groupedData(
 	weatherLocation,
 	unit = 'metric'
 ) {
-	// debugger;
+	debugger;
 	const weatherData = (await getWeatherFrom(weatherLocation, unit)).data;
-	const forecastData = (await weatherData).days;
+	const dailyData = (await weatherData).days;
+	const currentDay = 0;
+	const hourlyData = (await dailyData)[currentDay].hours;
 
-	const weatherInfo = getCurrentData(weatherData);
-	const forecastInfo = getForecastData(forecastData);
+	const current = getCurrentData(weatherData);
+	const forecast = getForecastData(dailyData);
+	const hourlyForecast = getHourlyData(hourlyData);
 
-	const weather = new CurrentWeather(
-		weatherInfo.temperature,
-		weatherInfo.condition,
-		weatherInfo.highTemp,
-		weatherInfo.lowTemp
-	);
+	const location = new Location(weatherData.address);
 
-	const location = new Location(weatherInfo.address, weather, forecastInfo);
+	const weather = Object.assign(location, current, hourlyForecast, forecast);
 
-	console.log(location);
+	console.log(weather);
 
-	return { location };
+	return { weather };
 };
