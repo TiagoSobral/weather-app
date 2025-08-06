8b3fc972d9e6ab0f0dc7c6de74834ba9a31c7963
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-06 18:32:29 +0100

    (refactor) Location, CurrentWeather
    (feat) DailyWeather, HourlyWeather
    
    -> new classes created in order to clean code,
    readability, and efficiency.
    
    -> location class only gets address and CurrentWeather
    gets the same as previously just changed some names
    for readability.
    
    -> see commit from functions

--- src/javascript/classes.js
+++ src/javascript/classes.js
@@ -1,16 +1,37 @@
 export class Location {
-	constructor(location, current, forecast) {
+	constructor(location, current, forecast, hourly) {
 		this.location = location;
 		this.current = current;
 		this.forecast = forecast;
+		this.hourly = hourly;
 	}
 }
 
 export class CurrentWeather {
-	constructor(temperature, condition, high, low) {
+	constructor(temperature, currentCondition, upcomingCondition, high, low) {
 		this.temperature = temperature;
-		this.condition = condition;
+		this.currentCondition = currentCondition;
+		this.upcomingCondition = upcomingCondition;
 		this.high = high;
 		this.low = low;
+	}
+}
+
+export class DailyWeather {
+	constructor(date, conditions, high, low) {
+		this.date = date;
+		this.conditions = conditions;
+		this.high = high;
+		this.low = low;
+	}
+}
+
+export class HourlyWeather {
+	constructor(hours, conditions, temperature) {
+		this.hours = hours;
+		this.conditions = conditions;
+		this.temperature = temperature;
+		// this.sunrise = sunrise;
+		// this.sunset = sunset;
 	}
 }
