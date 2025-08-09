74dc10106698552b8126bc08d23e1e2648cbe693
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-09 19:05:28 +0100

    (update) imports

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,6 +3,7 @@ import '../css/current.css';
 import '../css/hourly.css';
 import '../css/forecast.css';
 import '../css/search.css';
+import '../css/svgs.css';
 
 import { formListener } from './listeners';
 import { elementsVisibility } from './elements';
